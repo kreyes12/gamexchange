d6da1b28cc393b6bc7a9d3b85f3d60d647af29b4
Casey Van-Roose <caseyvanroose@Caseys-MacBook-Pro.local>
2018-08-21 17:34:54 +0100

    added new user form

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   has_many :user_games
   has_many :games, through: :user_games
 
+  validates :name, presence: true, uniqueness: true
+  validates :bio, presence:  true
+
   mount_uploader :avatar, AvatarUploader
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :games, only: [:index, :show]
   resources :genres, only: [:index, :show]
   resources :user_games, only: [:new, :edit]
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
