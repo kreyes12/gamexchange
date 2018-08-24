a5c2b5185599b8a94a77fba18c919a7b91e4643f
kreyes12 <karla.y.reyes12@gmail.com>
2018-08-24 09:05:29 +0100

    search functionality and wishlist link on user page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,13 +1,16 @@
 class GamesController < ApplicationController
 
   def index
+    if params[:game] and params[:game][:genre_id]
+      @games = Game.search(params[:game][:genre_id])
+    else
     @games = Game.all
-      if params[:query]
-        @games = Game.select {|game| game.genre.name == params[:query]}
-      else
-        @games = Game.all
+      #if params[:query]
+      #  @games = Game.select {|game| game.genre.name == params[:query]}
+      #else
+      #  @games = Game.all
       end
-    end
+  end
 
 
 
@@ -21,4 +24,10 @@ class GamesController < ApplicationController
     redirect_to wishlist_path(current_user)
   end
 
+end
+
+private
+
+def game_params
+  params.require(:game).permit(:title, :year_released, :genre_id)
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -5,4 +5,9 @@ class Game < ApplicationRecord
   has_many :consoles, through: :game_consoles
   belongs_to :genre
   has_many :wishlist_games
+
+  def self.search(search)
+    where("genre_id LIKE ?", "%#{search}")
+  end
+
 end
